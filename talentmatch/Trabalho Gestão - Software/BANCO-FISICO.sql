f8b3ad16eb46e2f69e85745ccb33e3870526dc03
lucasgbom <lucas.ribeiro42@aluno.ce.gov.br>
2025-04-29 11:23:25 -0300

    commitad

--- talentmatch/Trabalho Gestão - Software/BANCO-FISICO.sql
+++ talentmatch/Trabalho Gestão - Software/BANCO-FISICO.sql
@@ -1,63 +1,66 @@
-CREATE TABLE contratador (
-    nome VARCHAR(256),
-    senha VARCHAR(256),
-    endereco VARCHAR(256),
-    foto VARCHAR(256),
-    descricao VARCHAR(500),
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(256)
-);
-
-CREATE TABLE genero_musical (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(256),
-    id_artista INT
-);
-
-CREATE TABLE habilidade (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(256),
-    experiencia VARCHAR(256),
-    id_artista INT
-);
+START TRANSACTION;
 
 CREATE TABLE artista (
-    biografia VARCHAR(500),
-    senha VARCHAR(256),
+    id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(256),
-    nome_usuario VARCHAR(100);
-    foto_perfil VARCHAR(256),
+    nomeUsuario VARCHAR(100),
+    senha VARCHAR(256),
+    biografia VARCHAR(500),
+    fotoPerfil VARCHAR(256),
     endereco VARCHAR(256),
     disponivel BOOLEAN,
-    id INT AUTO_INCREMENT PRIMARY KEY,
     x VARCHAR(256),
     instagram VARCHAR(256),
     spotify VARCHAR(256),
     email VARCHAR(256)
 );
 
-CREATE TABLE projeto (
-    arquivo_caminho VARCHAR(256),
-    descricao VARCHAR(500),
-    id_artista INT,
+CREATE TABLE contratador (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    FOREIGN KEY(id_artista) REFERENCES artista (id)
+    nome VARCHAR(256),
+    senha VARCHAR(256),
+    endereco VARCHAR(256),
+    foto VARCHAR(256),
+    descricao VARCHAR(500),
+    email VARCHAR(256)
 );
 
-CREATE TABLE trabalho_post (
-    data_fim DATE,
+CREATE TABLE generoMusical (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(256),
+    idArtista INT,
+    FOREIGN KEY (idArtista) REFERENCES artista(id)
+);
+
+CREATE TABLE habilidade (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(256),
+    experiencia VARCHAR(256),
+    idArtista INT,
+    FOREIGN KEY (idArtista) REFERENCES artista(id)
+);
+
+CREATE TABLE projeto (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    arquivoCaminho VARCHAR(256),
+    descricao VARCHAR(500),
+    idArtista INT,
+    FOREIGN KEY (idArtista) REFERENCES artista(id)
+);
+
+CREATE TABLE trabalhoPost (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    dataInicio DATE,
+    dataFim DATE,
     requisitos VARCHAR(500),
-    data_inicio DATE,
     cache DOUBLE,
     descricao VARCHAR(256),
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    id_artista INT,
-    id_contratador INT,
-    id_habilidade INT,
-    FOREIGN KEY(id_artista) REFERENCES artista (id),
-    FOREIGN KEY(id_contratador) REFERENCES contratador (id),
-    FOREIGN KEY(id_habilidade) REFERENCES habilidade (id)
+    idArtista INT,
+    idContratador INT,
+    idHabilidade INT,
+    FOREIGN KEY (idArtista) REFERENCES artista(id),
+    FOREIGN KEY (idContratador) REFERENCES contratador(id),
+    FOREIGN KEY (idHabilidade) REFERENCES habilidade(id)
 );
 
-ALTER TABLE genero_musical ADD FOREIGN KEY(id_artista) REFERENCES artista (id);
-ALTER TABLE habilidade ADD FOREIGN KEY(id_artista) REFERENCES artista (id);
+COMMIT;
