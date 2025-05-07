7b46fe1e459615b20061cd803299fcfde0bebb49
lucasgbom <lucas.ribeiro42@aluno.ce.gov.br>
2025-05-07 20:32:09 -0300

    corte

--- talentmatch/app/Controller/ArtistaController.php
+++ talentmatch/app/Controller/ArtistaController.php
@@ -19,16 +19,8 @@ if(isset($_SESSION["usuario"])){
     
 
 $tipo = $_POST["tipo"];
-// Verifica se pesquisaram alguma coisa.
-/*
-if (isset($_GET['pesquisa']) && !empty($_GET['pesquisa'])) {
-    $artistas = $artistaDAO->buscar("id", $_GET['pesquisa']);
-} else {
-    $artistas = $artistaDAO->listarTodos();
-}*/
 
 if (isset($tipo)) {
-       
     switch ($tipo) {
         case 'cadastro_artista':
                 $artistaDAO->inserir($artista);
@@ -42,47 +34,5 @@ if (isset($tipo)) {
             $artistaDAO->atualizar($artista);
             break;
     }
-    /*
-    if ($tipo == "cadastro_artista") {
-        //$artista->setInstagram($u['instagram']);
-        //$artista->setSpotify($u['spotify']);
-        //$artista->setBiografia($u['biografia']);
-        //$artista->setFotoPerfil($u['foto_perfil']);
-        //$artista->setDisponivel($u['disponivel']);
-        //$artista->setX($u['x']);
-        //header("location: ../");
-        $artistaDAO->inserir($artista);
-    } else if ($tipo == "login_artista") {
-        $artistaDAO->logar($artista);
-    } else if ($tipo == "atualizar_artista") {
-        $artistaDAO->atualizar($artista);
-    }*/
 }
 
-// Cadastrar
-
-// Editar
-/*
-else if (isset($_POST['editar'])) {
-    $artista->setId($u['id']);
-    $artista->setBiografia($u['biografia']);
-    $artista->setSenha($u['senha']);
-    $artista->setNome($u['nome']);
-    $artista->setFotoPerfil($u['foto_perfil']);
-    $artista->setEndereco($u['endereco']);
-    $artista->setDisponivel($u['disponivel']);
-    $artista->setX($u['x']);
-    $artista->setInstagram($u['instagram']);
-    $artista->setSpotify($u['spotify']);
-    $artista->setEmail($u['email']);
-
-    $artistaDAO->atualizar($artista);
-    header("Location: ../../artista.php?msg=editado");
-}
-else if (isset($_GET['deletar'])) {
-    $artista->setId($_GET['deletar']);
-    $artistaDAO->deletar($artista);
-    header("Location: ../../artista.php?msg=apagado");
-} else {
-    header("Location: ../../artista.php?msg=erro");
-}*/
