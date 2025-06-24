5fc681f2d7378964843d61398f8f41ac945d59c5
lucasgbom <lucas.ribeiro42@aluno.ce.gov.br>
2025-06-23 22:36:04 -0300

    mapa quase deu errado

--- talentmatch/Trabalho Gestão - Software/BANCO-FISICO.sql
+++ talentmatch/Trabalho Gestão - Software/BANCO-FISICO.sql
@@ -1,47 +1,173 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 24/06/2025 às 03:26
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE usuario (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(256),
-    nomeUsuario VARCHAR(100),
-    senha VARCHAR(256),
-    biografia VARCHAR(512),
-    fotoPerfil VARCHAR(256),
-    endereco VARCHAR(256),
-    latitude DECIMAL(10, 8),  
-    longitude DECIMAL(11, 8) ,
-    email VARCHAR(256)
-);
 
-CREATE TABLE projeto (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    titulo VARCHAR(100),
-    arquivoCaminho VARCHAR(256),
-    descricao VARCHAR(500),
-    idUsuario INT,
-    FOREIGN KEY (idUsuario) REFERENCES usuario(id)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-CREATE TABLE post (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    titulo VARCHAR(100),
-    data_ DATE,
-    pagamento INT,
-    descricao VARCHAR(1280),
-    latitude DECIMAL(10, 8),  
-    longitude DECIMAL(11, 8),
-    idUsuario INT,
-    habilidade VARCHAR(100),
-    FOREIGN KEY (idUsuario) REFERENCES usuario(id)
-);
+--
+-- Banco de dados: `talentmatch`
+--
 
-CREATE TABLE usuario_post (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    idUsuario INT NOT NULL,
-    idPost INT NOT NULL,
-    UNIQUE KEY unique_usuario_post (idUsuario, idPost),
-    FOREIGN KEY (idUsuario) REFERENCES usuario(id),
-    FOREIGN KEY (idPost) REFERENCES post(id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+-- --------------------------------------------------------
 
+--
+-- Estrutura para tabela `post`
+--
+
+CREATE TABLE `post` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(100) DEFAULT NULL,
+  `data_` date DEFAULT NULL,
+  `pagamento` int(11) DEFAULT NULL,
+  `descricao` varchar(1280) DEFAULT NULL,
+  `latitude` decimal(10,8) DEFAULT NULL,
+  `longitude` decimal(11,8) DEFAULT NULL,
+  `idUsuario` int(11) DEFAULT NULL,
+  `habilidade` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `projeto`
+--
+
+CREATE TABLE `projeto` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(100) DEFAULT NULL,
+  `arquivoCaminho` varchar(256) DEFAULT NULL,
+  `descricao` varchar(500) DEFAULT NULL,
+  `idUsuario` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(256) DEFAULT NULL,
+  `telefone` varchar(100) DEFAULT NULL,
+  `senha` varchar(256) DEFAULT NULL,
+  `biografia` varchar(512) DEFAULT NULL,
+  `fotoPerfil` varchar(256) DEFAULT NULL,
+  `endereco` varchar(256) DEFAULT NULL,
+  `latitude` decimal(10,8) DEFAULT NULL,
+  `longitude` decimal(11,8) DEFAULT NULL,
+  `email` varchar(256) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuario_post`
+--
+
+CREATE TABLE `usuario_post` (
+  `id` int(11) NOT NULL,
+  `idUsuario` int(11) NOT NULL,
+  `idPost` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `post`
+--
+ALTER TABLE `post`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idUsuario` (`idUsuario`);
+
+--
+-- Índices de tabela `projeto`
+--
+ALTER TABLE `projeto`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idUsuario` (`idUsuario`);
+
+--
+-- Índices de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `usuario_post`
+--
+ALTER TABLE `usuario_post`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `unique_usuario_post` (`idUsuario`,`idPost`),
+  ADD KEY `idPost` (`idPost`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `post`
+--
+ALTER TABLE `post`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `projeto`
+--
+ALTER TABLE `projeto`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `usuario_post`
+--
+ALTER TABLE `usuario_post`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `post`
+--
+ALTER TABLE `post`
+  ADD CONSTRAINT `post_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`id`);
+
+--
+-- Restrições para tabelas `projeto`
+--
+ALTER TABLE `projeto`
+  ADD CONSTRAINT `projeto_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`id`);
+
+--
+-- Restrições para tabelas `usuario_post`
+--
+ALTER TABLE `usuario_post`
+  ADD CONSTRAINT `usuario_post_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`id`),
+  ADD CONSTRAINT `usuario_post_ibfk_2` FOREIGN KEY (`idPost`) REFERENCES `post` (`id`);
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
